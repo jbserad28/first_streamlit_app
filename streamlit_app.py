a41c2e4fdc42d567d14054ee95f59ff01ece3001
jbserad28 <115978014+jbserad28@users.noreply.github.com>
2023-03-06 20:08:22 +0800

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Oatmeal')
+streamlit.text('Smoothie')
+streamlit.text('Eggs')
