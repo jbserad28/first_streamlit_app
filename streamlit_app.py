4413d4ebab721d06de24325e661ec89007eb967f
jbserad28 <115978014+jbserad28@users.noreply.github.com>
2023-03-10 17:14:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ except URLError as e:
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("insert into fruit_load_list values ('kiwi')")
+my_cur.execute("insert into fruit_load_list values ('guava')")
 
 streamlit.stop()  
 streamlit.header("The fruit load list conatins:")
